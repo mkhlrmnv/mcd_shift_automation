504a4066e79d694b0c7de1d8b11bb68f6391468b
Mikhail Romanov <118537912+mkhlrmnv@users.noreply.github.com>
2023-08-10 14:08:51 +0300

    Update main.py
    main.py V2

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import defs
+import UI
 import os
 from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from googleapiclient.errors import HttpError
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']  #Google scope for calendar
 
-CLIENT_file = None #Your google OAuth2.0 .json file
+CLIENT_file = 'automation.json'  #Your google OAuth2.0 .json file
 
 creds = None
 
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)  #checks if user has already allowed program to use google api
+if os.path.exists('token.json'):#checks if user has already allowed program to use google api
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-if not creds  or not creds.valid:  #if user haven't done it yet pop-up window will apear where user has to allow program to use google api
-    if creds and creds.expired and creds.refresh_token:   
+if not creds  or not creds.valid:#if user haven't done it yet pop-up window will apear where user has to allow program to use google api
+    if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(CLIENT_file, SCOPES)
@@ -26,16 +26,18 @@ if not creds  or not creds.valid:  #if user haven't done it yet pop-up window wi
         token.write(creds.to_json())
 
 def main():
-    service = build('calendar', 'v3', credentials=creds)    #builds request for google calendar
+    ui = UI.RegistrationForm()
+    ui.run()  #open UI
     
-    scraper = defs.ShiftScraper()
-    scraper.get_credentials()  #logins into mcd shift website
-    scraper.login()
+    scraper = defs.ShiftScraper()  #runs class where are all the funktion
+    service = build('calendar', 'v3', credentials=creds)  #builds request for google calendar
     
-    shifts = scraper.get_shifts()  #gets shifts and dates
-    dates = scraper.get_dates()
+    scraper.login()  #logins into a website
     
-    scraper.quit()  #closes shift website
+    shifts = scraper.get_shifts()  #get list of shifts
+    dates = scraper.get_dates()  #get list on dates 
+    
+    scraper.quit()  #closes website
 
     x = 0
 
@@ -48,7 +50,7 @@ def main():
             
             date = scraper.change_format(dates[x])  #changes date format to Y.m.d from d.m.Y, it does it only here because i prefere d.m.Y format more
             
-            if end_hour < start_hour:   #if shift is continues over midnight shifts ending date changes
+            if end_hour < start_hour:  #if shift is continues over midnight shifts ending date changes
                 date_obj = datetime.strptime(date, "%Y-%m-%d")
                 next_day = date_obj + timedelta(days=1)
                 next_day_str = scraper.change_format(next_day.strftime("%d.%m.%Y"))
@@ -62,8 +64,8 @@ def main():
                         'timeZone': 'Europe/Helsinki',
                         },
                     }
-            else:   #creates event if it is not a night shift
-                event = {
+            else:
+                event = {  #creates event if it is not a night shift
                     'summary': 'Work',
                     'start': {'dateTime': f'{date}T{start_hour}:{start_minute}:00+03:00',
                         'timeZone': 'Europe/Helsinki',
@@ -72,9 +74,8 @@ def main():
                         'timeZone': 'Europe/Helsinki',
                         },
                     }
-
             e = service.events().insert(calendarId='primary', body=event).execute()  #sends event to google calendar
-            print ('Event created: %s' % (e.get('htmlLink')))   #prints errors
+            print ('Event created: %s' % (e.get('htmlLink')))  #prints errors
         x += 1
     
 if __name__ == "__main__":
