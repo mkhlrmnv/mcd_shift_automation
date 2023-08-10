e8a41d90453b376ab13f15a3409930945bf0cb26
Mikhail Romanov <118537912+mkhlrmnv@users.noreply.github.com>
2023-08-10 14:16:01 +0300

    Update defs.py
    defs.py V2

--- defs.py
+++ defs.py
@@ -2,41 +2,49 @@ import time
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import tkinter as tk
-from tkinter import simpledialog
+from tkinter import *
+import UI
+
 
 class ShiftScraper:
-    def __init__(self):
+    def __init__(self):  #opens selenium and right driver for it
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
-        self.username = None
-        self.password = None
+
+    def login(self):  #logins in to a website
+        self.driver.get(
+            "https://mcd.vuorolistat.fi/Default.aspx?ReturnUrl=/usershifts.aspx")
         
-    def get_credentials(self):   #with tkinter creates little window and asks for users username and password
-        root = tk.Tk()
-        root.withdraw()
-        self.username = simpledialog.askstring("Input", "Enter your username:")
-        self.password = simpledialog.askstring("Input", "Enter your password:", show="*")
-        root.destroy()
+        f = open("secrets.txt", "r+")
+        user = f.readline()  #from secret file reads username, password and selecion
+        passw = f.readline()
+        minne = f.readline()
 
-    def login(self):
-        self.driver.get("https://mcd.vuorolistat.fi/Default.aspx?ReturnUrl=/usershifts.aspx")  #opens finnish mcd shift website and logins into it
-
-        try:
-            self.driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys(self.username)
-            self.driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys(self.password)
-            self.driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
-            self.driver.find_element(By.CSS_SELECTOR, 'input[value="<"]').click()   #input[value="?"] can be < if you want to add previus "listajakso" shifts
-                                                                                    #nothing if now ongoing ones and > if if you want to add shifts in future
-        except:
+        try: 
+            self.driver.find_element(  #inserts username
+                By.CSS_SELECTOR, 'input[type="text"]').send_keys(user)
+            self.driver.find_element( #inserts password
+                By.CSS_SELECTOR, 'input[type="password"]').send_keys(passw)
+            #self.driver.find_element(
+                #By.XPATH, '//*[@id="wcmdLogin"]').click()
+            
+            match minne:  #depending what was selection clicks right button
+                case "<": x_path = '//*[@id="wcmdPreviousPeriod"]'
+                case "=": x_path = '//*[@id="aspnetForm"]/div[3]/table/tbody/tr/td[2]/div/div/div/button'
+                case ">": x_path = '//*[@id="wcmdNextPeriod"]'
+              
+            self.driver.find_element(By.XPATH, x_path).click()
+        except: #prints error if it accured 
             print("ERROR: Sisäänkirjautuminen epäonnistui")
+            
+        f.truncate(0)  #deletes username, password and selection from secret file
+            
 
-    def get_shifts(self):
-        time.sleep(1)  #scrapes all shifts from website and returns list on 21 days where -1 value represents days where are no shifts
-                        #and if there is any other value is timestamp of the shift
+    def get_shifts(self):  # gets list of shifts
+        time.sleep(1)
 
-        try:
+        try: #finds all elements with class name shifts
             all_shifts = self.driver.find_elements(By.CLASS_NAME, 'shifts')
         except:
             print("shifts not found")
@@ -44,24 +52,26 @@ class ShiftScraper:
             return None
 
         shift_list = []
-        for week in all_shifts:
-            try:
+        for week in all_shifts: #checks all above found elements for time stamps
+            try:                # if there is time stamps saves it into a list if not puts -1 in to a list
                 days = week.find_elements(By.XPATH, ".//td")
                 for shift in days:
                     try:
-                        shift_list.append(shift.find_element(By.XPATH, ".//a").text)
+                        shift_list.append(
+                            shift.find_element(By.XPATH, ".//a").text)
                     except:
                         shift_list.append(-1)
             except:
-                print("weeks not found")
+                print("weeks not found") #prints error if accured
 
         return shift_list
 
     def get_dates(self):
-        time.sleep(1)       #scrapes all dates from website and returns list of them
+        time.sleep(1)
 
-        try:
-            period = self.driver.find_element(By.XPATH, "//*[@id='ctl00_ctl00_cphMain_cphMain_wlblCurrentPeriod']").text
+        try:  #find starting date of shifts
+            period = self.driver.find_element(
+                By.XPATH, "//*[@id='ctl00_ctl00_cphMain_cphMain_wlblCurrentPeriod']").text
         except:
             print("start date no found")
             self.driver.quit()
@@ -70,31 +80,34 @@ class ShiftScraper:
         full_date = period.split(" - ")
         start_date = full_date[0]
 
-        def next_20(start):
+        def next_20(start): #puts next 20 days after starting date in to a list
             date_format = "%d.%m.%Y"
             star_date_obj = datetime.strptime(start, date_format)
-            next_20_list = [star_date_obj + timedelta(days=i) for i in range(21)]
+            next_20_list = [star_date_obj +
+                            timedelta(days=i) for i in range(21)]
             return [day.strftime(date_format) for day in next_20_list]
 
         days_list = next_20(start_date)
 
         return days_list
-    
-    def quit(self):  #method for closing website
+
+    def quit(self):
         self.driver.quit()
-    
+
     @staticmethod
-    def change_format(date):   #changes date format
+    def change_format(date):  #changes date format from d.m.Y to Y-m-d
         date = datetime.strptime(date, "%d.%m.%Y")
         return date.strftime("%Y-%m-%d")
 
+
 if __name__ == "__main__":
     scraper = ShiftScraper()
-    scraper.get_credentials()
+    ui = UI.RegistrationForm()
+    ui.run()
     scraper.login()
     shifts = scraper.get_shifts()
     dates = scraper.get_dates()
     scraper.quit()
-    
+
     print(shifts)
     print(dates)
