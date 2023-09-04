4b186500cdb3fe304f9a74ad66bc0d92349b6a3d
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-09-04 21:10:56 +0300

    Maked sure files matched original that works

--- defs.py
+++ defs.py
@@ -7,46 +7,47 @@ import UI
 
 
 class ShiftScraper:
-    def __init__(self):  #opens selenium and right driver for it
+    def __init__(self):  # opens selenium and right driver for it
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
 
-    def login(self):  #logins in to a website
+    def login(self):  # logins in to a website
         self.driver.get(
             "https://mcd.vuorolistat.fi/Default.aspx?ReturnUrl=/usershifts.aspx")
-        
+
         f = open("secrets.txt", "r+")
-        user = f.readline()  #from secret file reads username, password and selecion
+        user = f.readline()  # from secret file reads username, password and selecion
         passw = f.readline()
         minne = f.readline()
 
-        try: 
-            self.driver.find_element(  #inserts username
+        try:
+            self.driver.find_element(  # inserts username
                 By.CSS_SELECTOR, 'input[type="text"]').send_keys(user)
-            self.driver.find_element( #inserts password
+            self.driver.find_element(  # inserts password
                 By.CSS_SELECTOR, 'input[type="password"]').send_keys(passw)
             self.driver.find_element(
                 By.XPATH, '//*[@id="tabs"]/li[2]')
-            #self.driver.find_element(
-                #By.XPATH, '//*[@id="wcmdLogin"]').click()
-            
-            match minne:  #depending what was selection clicks right button
+            # self.driver.find_element(
+            # By.XPATH, '//*[@id="wcmdLogin"]').click()
+
+            match minne:  # depending what was selection clicks right button
                 case "<\n": x_path = '//*[@id="wcmdPreviousPeriod"]'
                 case "=\n": x_path = '//*[@id="aspnetForm"]/div[3]/table/tbody/tr/td[2]/div/div/div/button'
                 case ">\n": x_path = '//*[@id="wcmdNextPeriod"]'
-              
+
             self.driver.find_element(By.XPATH, x_path).click()
-        except: #prints error if it accured 
+        except:  # prints error if it accured
             print("ERROR: Sisäänkirjautuminen epäonnistui")
-            
-        if int(f.readline()) == 0: #if user selected not to remember his credentials
-            f.truncate(0)  #deletes username, password and selection from secret file           
+
+        if int(f.readline()) == 0:  # if user selected not to remember his credentials
+            # deletes username, password and selection from secret file
+            f.truncate(0)
 
     def get_shifts(self):  # gets list of shifts
         time.sleep(1)
 
-        try: #finds all elements with class name shifts
+        try:  # finds all elements with class name shifts
             all_shifts = self.driver.find_elements(By.CLASS_NAME, 'shifts')
         except:
             print("shifts not found")
@@ -54,7 +55,7 @@ class ShiftScraper:
             return None
 
         shift_list = []
-        for week in all_shifts: #checks all above found elements for time stamps
+        for week in all_shifts:  # checks all above found elements for time stamps
             try:                # if there is time stamps saves it into a list if not puts -1 in to a list
                 days = week.find_elements(By.XPATH, ".//td")
                 for shift in days:
@@ -67,14 +68,14 @@ class ShiftScraper:
                     except:
                         shift_list.append(-1)
             except:
-                print("weeks not found") #prints error if accured
+                print("weeks not found")  # prints error if accured
 
         return shift_list
 
     def get_dates(self):
         time.sleep(1)
 
-        try:  #find starting date of shifts
+        try:  # find starting date of shifts
             period = self.driver.find_element(
                 By.XPATH, "//*[@id='ctl00_ctl00_cphMain_cphMain_wlblCurrentPeriod']").text
         except:
@@ -85,7 +86,7 @@ class ShiftScraper:
         full_date = period.split(" - ")
         start_date = full_date[0]
 
-        def next_20(start): #puts next 20 days after starting date in to a list
+        def next_20(start):  # puts next 20 days after starting date in to a list
             date_format = "%d.%m.%Y"
             star_date_obj = datetime.strptime(start, date_format)
             next_20_list = [star_date_obj +
@@ -100,7 +101,7 @@ class ShiftScraper:
         self.driver.quit()
 
     @staticmethod
-    def change_format(date):  #changes date format from d.m.Y to Y-m-d
+    def change_format(date):  # changes date format from d.m.Y to Y-m-d
         date = datetime.strptime(date, "%d.%m.%Y")
         return date.strftime("%Y-%m-%d")
 
@@ -116,4 +117,3 @@ if __name__ == "__main__":
 
     print(shifts)
     print(dates)
-
--- main.py
+++ main.py
@@ -7,76 +7,85 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']  #Google scope for calendar
+# Google scope for calendar
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-CLIENT_file = 'automation.json'  #Your google OAuth2.0 .json file
+CLIENT_file = 'automation.json'  # Your google OAuth2.0 .json file
 
 creds = None
 
-if os.path.exists('token.json'):#checks if user has already allowed program to use google api
+# checks if user has already allowed program to use google api
+if os.path.exists('token.json'):
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-if not creds  or not creds.valid:#if user haven't done it yet pop-up window will apear where user has to allow program to use google api
+if not creds or not creds.valid:  # if user haven't done it yet pop-up window will apear where user has to allow program to use google api
     if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())  # uus kommentti
+        creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(CLIENT_file, SCOPES)
         creds = flow.run_local_server(port=0)
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
 
+
 def main():
     ui = UI.RegistrationForm()
-    ui.run()  #open UI
-    
-    scraper = defs.ShiftScraper()  #runs class where are all the funktion
-    service = build('calendar', 'v3', credentials=creds)  #builds request for google calendar
-    
-    scraper.login()  #logins into a website
-    
-    shifts = scraper.get_shifts()  #get list of shifts
-    dates = scraper.get_dates()  #get list on dates 
-    
-    scraper.quit()  #closes website
+    ui.run()  # open UI
+
+    scraper = defs.ShiftScraper()  # runs class where are all the funktion
+    # builds request for google calendar
+    service = build('calendar', 'v3', credentials=creds)
+
+    scraper.login()  # logins into a website
+
+    shifts = scraper.get_shifts()  # get list of shifts
+    dates = scraper.get_dates()  # get list on dates
+
+    scraper.quit()  # closes website
 
     x = 0
 
-    for i in shifts:  #for every date in three week period checks if there is a shift
-        if i != -1:  #if there is creates event and sends it to google calendar
+    for i in shifts:  # for every date in three week period checks if there is a shift
+        if i != -1:  # if there is creates event and sends it to google calendar
             start_hour = str(i[:2])
-            start_minute = str(i[2:4])  #gets starting hour, minute etc.
+            start_minute = str(i[2:4])  # gets starting hour, minute etc.
             end_hour = str(i[5:7])
             end_minute = str(i[7:9])
-            
-            date = scraper.change_format(dates[x])  #changes date format to Y.m.d from d.m.Y, it does it only here because i prefere d.m.Y format more
-            
-            if end_hour < start_hour:  #if shift is continues over midnight shifts ending date changes
+
+            # changes date format to Y.m.d from d.m.Y, it does it only here because i prefere d.m.Y format more
+            date = scraper.change_format(dates[x])
+
+            if end_hour < start_hour:  # if shift is continues over midnight shifts ending date changes
                 date_obj = datetime.strptime(date, "%Y-%m-%d")
                 next_day = date_obj + timedelta(days=1)
-                next_day_str = scraper.change_format(next_day.strftime("%d.%m.%Y"))
-                
-                event = {  #creates event if it is a night shift
+                next_day_str = scraper.change_format(
+                    next_day.strftime("%d.%m.%Y"))
+
+                event = {  # creates event if it is a night shift
                     'summary': 'Work',
                     'start': {'dateTime': f'{date}T{start_hour}:{start_minute}:00+03:00',
-                        'timeZone': 'Europe/Helsinki',
-                        },
+                              'timeZone': 'Europe/Helsinki',
+                              },
                     'end': {'dateTime': f'{next_day_str}T{end_hour}:{end_minute}:00+03:00',
-                        'timeZone': 'Europe/Helsinki',
-                        },
-                    }
+                            'timeZone': 'Europe/Helsinki',
+                            },
+                }
             else:
-                event = {  #creates event if it is not a night shift
+                event = {  # creates event if it is not a night shift
                     'summary': 'Work',
                     'start': {'dateTime': f'{date}T{start_hour}:{start_minute}:00+03:00',
-                        'timeZone': 'Europe/Helsinki',
-                        },
+                              'timeZone': 'Europe/Helsinki',
+                              },
                     'end': {'dateTime': f'{date}T{end_hour}:{end_minute}:00+03:00',
-                        'timeZone': 'Europe/Helsinki',
-                        },
-                    }
-            e = service.events().insert(calendarId='primary', body=event).execute()  #sends event to google calendar
-            print ('Event created: %s' % (e.get('htmlLink')))  #prints errors
+                            'timeZone': 'Europe/Helsinki',
+                            },
+                }
+            # sends event to google calendar
+            e = service.events().insert(calendarId='primary', body=event).execute()
+            # print(event)
+            print('Event created: %s' % (e.get('htmlLink')))  # prints errors
         x += 1
-    
+
+
 if __name__ == "__main__":
     main()
